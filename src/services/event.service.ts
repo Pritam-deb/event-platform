6549ae36a3c6867de460e775b5bbf0eb1fc5b069
Pritam Debnath <pritamdebnath297@gmail.com>
2025-12-23 15:21:15 +0530

    created service layer for events

---
+++ src/services/event.service.ts
@@ -0,0 +1,58 @@
+import { db } from '@/db';
+import { events } from '@/db/schema/events';
+import { eq } from 'drizzle-orm';
+import {
+    CreateEventInput,
+    UpdateEventInput,
+} from '@/types/event';
+
+
+export const createEvent = async (input: CreateEventInput) => {
+    const id = crypto.randomUUID();
+
+    await db.insert(events).values({
+        id,
+        title: input.title,
+        description: input.description ?? null,
+        location: input.location ?? null,
+        totalTickets: input.totalTickets,
+        eventStatus: input.eventStatus,
+        eventType: input.eventType,
+        eventTag: input.eventTag ?? null,
+        startAt: input.startAt,
+        endAt: input.endAt,
+        isPublished: input.isPublished ?? false,
+    });
+
+    return id;
+};
+
+
+export const getAllEvents = async () => {
+    return db.select().from(events);
+};
+
+export const getEventById = async (id: string) => {
+    const result = await db
+        .select()
+        .from(events)
+        .where(eq(events.id, id));
+
+    return result[0] ?? null;
+};
+
+export const updateEvent = async (
+    id: string,
+    input: UpdateEventInput
+) => {
+    await db
+        .update(events)
+        .set({
+            ...input,
+        })
+        .where(eq(events.id, id));
+};
+
+export const deleteEvent = async (id: string) => {
+    await db.delete(events).where(eq(events.id, id));
+};
---
+++ src/types/event.ts
@@ -0,0 +1,33 @@
+export type Event = {
+    id: string;
+    title: string;
+    description: string | null;
+    location: string | null;
+    ticketSold: number;
+    totalTickets: number;
+    eventStatus: string;
+    eventType: string;
+    eventTag: string | null;
+    eventAttendees: number;
+    eventRevenue: string;
+    startAt: Date;
+    endAt: Date;
+    isPublished: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type CreateEventInput = {
+    title: string;
+    description?: string;
+    location?: string;
+    totalTickets: number;
+    eventStatus: string;
+    eventType: string;
+    eventTag?: string;
+    startAt: Date;
+    endAt: Date;
+    isPublished?: boolean;
+};
+
+export type UpdateEventInput = Partial<CreateEventInput>;
